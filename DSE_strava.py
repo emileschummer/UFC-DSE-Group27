d16394f5a7fdcee2567450912e6fd90e51e40444
Tijn_Pellikaan <t.l.h.pellikaan@student.tudelft.nl>
2025-05-12 10:12:19 +0200

    .

--- DSE_strava.py
+++ DSE_strava.py
@@ -3,9 +3,25 @@ import gpxpy
 # Load the GPX file
 with open('Lunch_Run.gpx', 'r') as gpx_file:
     gpx = gpxpy.parse(gpx_file)
-
+bike_data = []
 # Extract data
 for track in gpx.tracks:
     for segment in track.segments:
-        for point in segment.points:
-            print(f"Time: {point.time}, Latitude: {point.latitude}, Longitude: {point.longitude}, Elevation: {point.elevation}")
+        points = segment.points
+        if not points:
+            continue
+        
+        # Get reference start time
+        start_time = points[0].time
+
+        for point in points:
+            time_delta = (point.time - start_time).total_seconds()  # Time difference in seconds
+            bike_data.append([
+                time_delta,            # Time since start in seconds
+                point.latitude,        # Latitude
+                point.longitude,       # Longitude
+                point.elevation  ]      # Elevation
+            )
+
+
+print(bike_data) #time since start (s), latitude, longitude, elevation
