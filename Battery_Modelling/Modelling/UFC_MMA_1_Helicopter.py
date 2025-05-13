c98fc1dafc4e1801bcca426c85246f71e411b573
Tijn_Pellikaan <t.l.h.pellikaan@student.tudelft.nl>
2025-05-13 09:17:14 +0200

    .

--- Battery_Modelling/Modelling/UFC_MMA_1_Helicopter.py
+++ Battery_Modelling/Modelling/UFC_MMA_1_Helicopter.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-CD = 0.105 #https://dspace-erf.nlr.nl/server/api/core/bitstreams/0a756857-3708-4250-9524-bdbcc0020d33/content
+CD = 1.05 #https://dspace-erf.nlr.nl/server/api/core/bitstreams/0a756857-3708-4250-9524-bdbcc0020d33/content
 S = 0.3
 W = 250 #N
 
