1a412c54ef046bace5ac848c0b8b7936b5564b17
Roan-2003 <R.R.Raben@student.tudelft.nl>
2025-05-13 14:56:54 +0200

    Unit3 done

---
+++ Battery_Modelling/Verification/test_UNIT_03.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+# Add the parent directory of 'Battery_Modelling' to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+import pytest as pt
+import numpy as np
+from Battery_Modelling.Modelling.UFC_MMA_2_Quad import calculate_power_UFC_MMA_2
+
+# Test parameters
+CD = 0.105 #https://dspace-erf.nlr.nl/server/api/core/bitstreams/0a756857-3708-4250-9524-bdbcc0020d33/content
+
+W = 250 #N
+
+A = 0.3
+eta = 0.8
+totalA = (1.041/2)**2*np.pi
+numberengines = 4
+A = totalA/numberengines
+S = 0.16
+expected_power = 3428.961414 
+def test_calculate_power_UFC_MMA_2():
+    """
+    Test the calculate_power_UFC_MMA_1 function.
+    """
+    
+    # Call the function with the test input
+    result = calculate_power_UFC_MMA_2(np.pi/9, 10, 1.225)
+
+    # Assert that the result is close to the expected value
+    assert pt.approx(result, rel=1e-1) == expected_power
