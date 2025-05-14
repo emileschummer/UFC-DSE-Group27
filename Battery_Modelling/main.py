ace53bcbc40dfd56bc9a0e024b0015fd169df80e
Roan-2003 <R.R.Raben@student.tudelft.nl>
2025-05-14 10:27:06 +0200

    99% babyyyyyyy

--- Battery_Modelling/main.py
+++ Battery_Modelling/main.py
@@ -4,7 +4,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 
 from Battery_Modelling.Modelling.races import *
 
-if __name__ == "__main__":
+def main():
     flat_race()
     get_race_results("Battery_Modelling/Output")
     plot_power_vs_velocity()
+
+if __name__ == "__main__":
+    main()
