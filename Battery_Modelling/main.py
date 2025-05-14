bf8f7c8ba44f0256945e7eafcbebe02784387ebb
Emile Schummer <emileschummer@hotmail.fr>
2025-05-14 16:12:52 +0200

    Working Sensitivity Analysis for 7h races

--- Battery_Modelling/main.py
+++ Battery_Modelling/main.py
@@ -8,8 +8,8 @@ from Sensitivity_Analysis.plot_power import *
 def main():
     #flat_race() 
     #plot_race_results("Battery_Modelling/Output")
-    plot_power_vs_velocity_sensitivity()
-    get_race_results(iterations=10)   
+    #plot_power_vs_velocity_sensitivity()
+    get_race_results("Battery_Modelling/Output",iterations=5)#50)   
 
 if __name__ == "__main__":
     main()
