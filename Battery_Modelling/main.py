2eefdfe769f0a118e44985dbf8f1a04c1f526890
Roan-2003 <R.R.Raben@student.tudelft.nl>
2025-05-14 16:18:17 +0200

    Oke

--- Battery_Modelling/main.py
+++ Battery_Modelling/main.py
@@ -3,4 +3,5 @@ from Sensitivity_Analysis.plot_power import *
 
 if __name__ == "__main__":
     # Example usage
-    print(flat_race())
+    plot_power_vs_velocity_sensitivity(slope=0, iterations=10, variance=0.1)
+
