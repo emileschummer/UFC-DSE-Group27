1cbefe0ef5793b07be573e03e43fb3a9a8417a69
Roan-2003 <R.R.Raben@student.tudelft.nl>
2025-05-14 16:23:47 +0200

    Should be fine

--- Battery_Modelling/Modelling/UFC_MMA_1_Helicopter.py
+++ Battery_Modelling/Modelling/UFC_MMA_1_Helicopter.py
@@ -5,5 +5,5 @@ def calculate_power_UFC_MMA_1(incline,V,rho, inputs):
     Tvertical = np.cos(incline)*inputs[0]
     Thorizontal = D + np.sin(incline)*inputs[0]
     T = (Tvertical**2 + Thorizontal**2)**0.5
-    P = (abs(T)**3/(2*rho*inputs[5]))**0.5/inputs[1]
+    P = (abs(T)**3/(2*rho*inputs[4]))**0.5/inputs[1]
     return P
