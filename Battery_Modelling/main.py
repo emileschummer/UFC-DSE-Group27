ead0925b5040d6aa9b10d8ebf7f6e5f399a0ac16
Roan-2003 <R.R.Raben@student.tudelft.nl>
2025-05-14 09:58:49 +0200

    YEah not good yet

--- Battery_Modelling/main.py
+++ Battery_Modelling/main.py
@@ -1,6 +1,10 @@
-from Modelling.races import *
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from Battery_Modelling.Modelling.races import *
 
 if __name__ == "__main__":
-    #flat_race()
-    #get_race_results("Battery_Modelling/Output")
+    flat_race()
+    get_race_results("Battery_Modelling/Output")
     plot_power_vs_velocity()
