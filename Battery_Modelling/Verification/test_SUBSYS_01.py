a7f8e4acfce2cf1a495fe5254484cb8aab20eef6
Roan-2003 <R.R.Raben@student.tudelft.nl>
2025-05-15 10:18:55 +0200

    SUbsystem tests names changed

--- Battery_Modelling/Verification/test_SUBSYS_01.py
+++ Battery_Modelling/Verification/test_SUBSYS_01.py
@@ -31,21 +31,3 @@ def test_plot_race_results(mock_show, mock_savefig, mock_make_races):
     mock_savefig.assert_called_once()
     mock_show.assert_called_once()
 
-#Test flat_race function
-@patch("Battery_Modelling.Modelling.races.calculate_power_UFC_MMA_1", return_value=10)
-@patch("Battery_Modelling.Modelling.races.calculate_power_UFC_MMA_2", return_value=20)
-@patch("Battery_Modelling.Modelling.races.calculate_power_UFC_MMA_3", return_value=30)
-@patch("Battery_Modelling.Modelling.races.calculate_power_UFC_MMA_4", return_value=40)
-def test_flat_race_output(mock_p1, mock_p2, mock_p3, mock_p4, capsys):
-
-    flat_race()
-
-    captured = capsys.readouterr()
-
-    assert "---------7h Flat Race at 50km/h---------" in captured.out
-    assert "UFC-MMA-1 Helicopter Energy (Wh):  " in captured.out
-    assert "UFC-MMA-2 Quadcopter Energy (Wh):  " in captured.out
-    assert "UFC-MMA-3 Osprey Energy (Wh):  " in captured.out
-    assert "UFC-MMA-4 Yangda Energy (wh):  " in captured.out
-
-
--- Battery_Modelling/Verification/test_SUBSYS_04.py
+++ Battery_Modelling/Verification/test_SUBSYS_04.py
@@ -10,18 +10,6 @@ from unittest.mock import patch, MagicMock
 import numpy as np
 import pandas as pd
 
-#Test for plot_power_vs_velocity_sensitivity function
-@patch("Battery_Modelling.Sensitivity_Analysis.plot_power.calculate_power_UFC_MMA_1", return_value=10)
-@patch("Battery_Modelling.Sensitivity_Analysis.plot_power.calculate_power_UFC_MMA_2", return_value=20)
-@patch("Battery_Modelling.Sensitivity_Analysis.plot_power.calculate_power_UFC_MMA_3", return_value=30)
-@patch("Battery_Modelling.Sensitivity_Analysis.plot_power.calculate_power_UFC_MMA_4", return_value=40)
-@patch("Battery_Modelling.Sensitivity_Analysis.plot_power.plt.plot")
-@patch("Battery_Modelling.Sensitivity_Analysis.plot_power.plt.show")
-def test_plot_power_vs_velocity_sensitivity(mock_show, mock_plot, mock_p1, mock_p2, mock_p3, mock_p4):
-    plot_power_vs_velocity_sensitivity(iterations=2)
-    assert mock_plot.called
-    assert mock_show.called
-
 #Test get_race_results function
 @patch("Battery_Modelling.Sensitivity_Analysis.plot_power.calculate_power_UFC_MMA_1", return_value=100)
 @patch("Battery_Modelling.Sensitivity_Analysis.plot_power.calculate_power_UFC_MMA_2", return_value=200)
