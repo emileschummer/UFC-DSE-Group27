beb632ac4d1e5f0a4879a479713304861dcd2679
Jadon Cooper <j.k.cooper@student.tudelft.nl>
2025-05-19 16:41:19 +0200

    I might be him

--- Sensitivity Analysis.py
+++ Sensitivity Analysis.py
@@ -14,53 +14,44 @@ data = {
 }
 
 # Create the DataFrame
-df = pd.DataFrame(data)
-df.set_index("Criteria", inplace=True)
-
-
-
-
-
-def Vary_Individual(Weights, Category, Amount):
-    WeightsMax = Weights.loc[Category] + Amount
-    WeightsMin = Weights.loc[Category] - Amount
-    Adjusted_Weights = [WeightsMax, WeightsMin]
-
-    return(Adjusted_Weights)
-
-
-def Vary_weights(Amount, Row):
-    WeightsMax = df.loc[Row,"Weight (%)"] + Amount
-    WeightsMin = df.loc[Row,"Weight (%)"] - Amount
-    Adjusted_Weights = [WeightsMax, WeightsMin]
-
-    return Adjusted_Weights
-
+df = pd.DataFrame(data).set_index("Criteria")
+print(df)
 
 
 
 def Compare():
+    #resetting the table
+    data = {
+        "Criteria": ["Endurance", "Complexity", "Quality", "Cost", "Safety", "Noise", "Sustainability"],
+        "Weight (%)": [0.10, 0.15, 0.15, 0.20, 0.10, 0.10, 0.20],
+        "Helicopter": [0.10, 0.22, 0.65, 0.37, 0.34, 0.10, 0.22],
+        "Quadcopter": [0.10, 1, 1, 0.37, 0.70, 0.75, 0.37],
+        "Osprey": [0.50, 0.38, 0.52, 0.83, 0.75, 1, 0.38],
+        "Yangda": [0.75, 0.75, 0.65, 1, 0.90, 1, 0.58],
+    }
+    #global df
+    df = pd.DataFrame(data)
+    df.set_index("Criteria", inplace=True)
+
     # print("\n Original Table:")
     # print(df)
 
     #Seperating
     Pre_weights = df["Weight (%)"].copy()
-    Heli = df["Helicopter"]
-    Quad = df["Quadcopter"]
-    Tiltrotor = df["Osprey"]
-    Yangda = df["Yangda"]
+    Heli = df["Helicopter"].copy()
+    Quad = df["Quadcopter"].copy()
+    Tiltrotor = df["Osprey"].copy()
+    Yangda = df["Yangda"].copy()
 
     # Compute weighted total score 
-    PRE_Quad_total = (Quad * Pre_weights).sum()
     PRE_Heli_total = (Heli*Pre_weights).sum()
+    PRE_Quad_total = (Quad * Pre_weights).sum()
     PRE_Tilt_total = (Tiltrotor*Pre_weights).sum()
     PRE_Yangda_total = (Yangda*Pre_weights).sum()
-    PRE_Totals = [PRE_Quad_total,PRE_Heli_total,PRE_Tilt_total,PRE_Yangda_total]
+    PRE_Totals = [PRE_Heli_total,PRE_Quad_total,PRE_Tilt_total,PRE_Yangda_total]
 
     # print("\n The pre Totals are:")
     # print(PRE_Quad_total,PRE_Heli_total,PRE_Tilt_total,PRE_Yangda_total)
-
-
 
 
     # Randomly pick two different row Categories
@@ -87,11 +78,11 @@ def Compare():
 
     # Compute weighted total score 
     Post_weights = df["Weight (%)"]
-    POST_Quad_total = (Quad *Post_weights ).sum()
     POST_Heli_total = (Heli*Post_weights).sum()
+    POST_Quad_total = (Quad *Post_weights ).sum()
     POST_Tilt_total = (Tiltrotor*Post_weights).sum()
     POST_Yangda_total = (Yangda*Post_weights).sum()
-    POST_Totals = [POST_Quad_total,POST_Heli_total,POST_Tilt_total,POST_Yangda_total]
+    POST_Totals = [POST_Heli_total,POST_Quad_total,POST_Tilt_total,POST_Yangda_total]
 
 
     # print("\n The new post weights are:")
@@ -100,13 +91,14 @@ def Compare():
     # print("\nWeight COmparison:")
     # print(Pre_weights, Post_weights)
 
+    print(PRE_Totals)
     return PRE_Totals, POST_Totals
 
 
 #MAIN - I guess
-for i in range(100):
+for i in range(1000):
     Pre,Post = Compare()
-    if max(Post) >= 1:
-        print("FUCK")
+    if max(Pre) >= 1:
+        print("pAST 100% eRROR")
         print(max(Post))
 
