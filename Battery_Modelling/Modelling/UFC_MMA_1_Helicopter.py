e80d3ff01162e15c42bd096e11e20ee6a6cfc76d
Emile Schummer <emileschummer@hotmail.fr>
2025-05-20 18:18:28 +0200

    very final updated version, working rotor model!!!

--- Battery_Modelling/Modelling/UFC_MMA_1_Helicopter.py
+++ Battery_Modelling/Modelling/UFC_MMA_1_Helicopter.py
@@ -21,5 +21,38 @@ def calculate_power_UFC_MMA_1(incline,V,rho, inputs):
     P_profile = (inputs[6]*inputs[7]*rho*inputs[8]*inputs[9]**3*inputs[4]**4*(1+3*(V/(inputs[9]*inputs[4]))**2))/8
     P = (P_induced + P_parasite + P_profile) / inputs[1]
     return P
+"""
+import matplotlib.pyplot as plt
+
+velocities = np.linspace(0, 100, 100)
+incline = 0
+rho = 1.225
+inputs = [W,eta,CD0_MMA1,S_parasite_MMA1,r_MMA1,A_prop_MMA1,N_blades_MMA1,chord_blade_MMA1,cd_blade_MMA1,omega_MMA1,solidity_MMA1]
+
+P_list = []
+P_induced_list = []
+P_parasite_list = []
+P_profile_list = []
+
+for V in velocities:
+    try:
+        P, P_induced, P_parasite, P_profile = calculate_power_UFC_MMA_1(incline, V, rho, inputs)
+    except Exception:
+        P, P_induced, P_parasite, P_profile = np.nan, np.nan, np.nan, np.nan
+    P_list.append(P)
+    P_induced_list.append(P_induced)
+    P_parasite_list.append(P_parasite)
+    P_profile_list.append(P_profile)
+
+plt.plot(velocities, P_induced_list, label='P_induced')
+plt.plot(velocities, P_parasite_list, label='P_parasite')
+plt.plot(velocities, P_profile_list, label='P_profile')
+plt.plot(velocities, P_list, label='P (Total)')
+plt.xlabel('Velocity (m/s)')
+plt.ylabel('Power (W)')
+plt.title('Power vs Velocity')
+plt.legend()
+plt.grid(True)
+plt.show()"""
 
 
--- Battery_Modelling/Modelling/UFC_MMA_3_Osprey.py
+++ Battery_Modelling/Modelling/UFC_MMA_3_Osprey.py
@@ -1,7 +1,6 @@
 import numpy as np
 from Battery_Modelling.Input.Configuration_inputs import largest_real_positive_root
 
-numberengines_MMA3=2
 def calculate_power_UFC_MMA_3(incline, V, rho, inputs):
     L = np.cos(incline)*inputs[0]
     if V >0:
@@ -21,10 +20,10 @@ def calculate_power_UFC_MMA_3(incline, V, rho, inputs):
         L = 0.5*rho*CL*inputs[4]*V**2 *inputs[7]
         D_parasite = 0.5*rho*CD*inputs[4]*V**2 #D_parasite includes both the parasite drag of the fusealge and the drag of the lifting force of the wing
         #Calculate thrust and power per engine
-        Tvertical = (np.cos(incline)*inputs[0] - L)/numberengines_MMA3
-        Thorizontal = (D_parasite + np.sin(incline)*inputs[0])/numberengines_MMA3
+        Tvertical = (np.cos(incline)*inputs[0] - L)/inputs[15]
+        Thorizontal = (D_parasite + np.sin(incline)*inputs[0])/inputs[15]
         T = (Tvertical**2 + Thorizontal**2)**0.5
-        A_prop = inputs[9]/numberengines_MMA3
+        A_prop = inputs[9]/inputs[15]
         #Solve for vi
         alpha_T= np.arccos(Tvertical/T)
         A=4*(rho*A_prop)**2
@@ -35,7 +34,7 @@ def calculate_power_UFC_MMA_3(incline, V, rho, inputs):
         vi_roots = np.roots([A,B,C,D,E])
         vi = largest_real_positive_root(vi_roots)
         #Calculate Total Powers
-        P_induced = vi * T * numberengines_MMA3
+        P_induced = vi * T * inputs[15]
         P_parasite = D_parasite * V #normal thrust of an aircraft
         P_profile = (inputs[10]*inputs[11]*rho*inputs[12]*inputs[13]**3*inputs[6]**4*(1+3*(V/(inputs[13]*inputs[6]))**2))/8
         P = (P_induced + P_parasite + P_profile) / inputs[1]
