444387d6f71e159f3c56819523d670f8dd9b4e4b
Tijn_Pellikaan <t.l.h.pellikaan@student.tudelft.nl>
2025-06-05 11:38:01 +0200

    .

---
+++ Tijn/Tail_sizing.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+stability_margin = 0.05
+Clalpha = 4.635
+Clhalpha = 4.158
+Clmax = 2.4
+Clhmax = 1.5
+Cmac = -0.5
+S = 1
+c = 0.36
+lh = 1
+ARh = 6
+show = True
+def get_tail(show,Clalpha,Clhalpha,Clmax,Clhmax,Cmac,S,c,lh,ARh):
+
+
+
+
+
+    def stability_curve(Sh):
+        l_c = (Clhalpha/Clalpha)*(Sh*(lh))/(S*c) - stability_margin
+        return l_c
+    def control_curve(Sh):
+        l_c = - Cmac/Clmax - Clhmax/Clmax*(Sh*(lh))/(S*c)
+        return l_c
+
+
+    surface = np.linspace(0,1,100)
+    stab = []
+    contr = []
+
+    for Sh in surface:
+        stab.append(stability_curve(Sh))
+        contr.append(control_curve(Sh))
+        
+    if show:
+        plt.plot(stab,surface)
+        plt.plot(contr,surface)
+        plt.xlabel('(Xcg - Xac)/c')       # X-axis label
+        plt.ylabel('Sh/S')   # Y-axis label
+        plt.show()
+    for i in range(len(surface)):
+        margin = stab[i] - contr[i]
+        if margin > 0:
+            print(margin*c)
+            print(surface[i])
+            break
+    bh = (ARh*surface[i])**0.5
+    ch = surface[i]/bh
+    return margin*c, surface[i], bh, ch
+print(get_tail(True,Clalpha,Clhalpha,Clmax,Clhmax,Cmac,S,c,lh,ARh))
