0ca581ccef0a0d37babcc1ac88ed2faa4d97419a
Roan-2003 <R.R.Raben@student.tudelft.nl>
2025-06-13 16:36:22 +0200

    Emile look at this

--- Final_UAV_Sizing/Modelling/Propeller_and_Battery_Sizing/Model/UFC_FC_YEAH.py
+++ Final_UAV_Sizing/Modelling/Propeller_and_Battery_Sizing/Model/UFC_FC_YEAH.py
@@ -7,27 +7,27 @@ import matplotlib.pyplot as plt
 from Input.fixed_input_values import g
 import Input.RaceData.Strava_input_csv as sva
 
-def flat_plate_drag_coefficient(V, rho, h, L):
-    if V>0:
+def flat_plate_drag_coefficient(V, rho, h, S_wing, L, w):
+    if V <= 0:
+        Cf_i = 0
+    else:
         T0 = 288.15
         T = T0 + -0.0065 * h
         Re= rho * V * L / 1.81e-5  # Reynolds number, assuming a kinematic viscosity of air at sea level
         a= np.sqrt(1.4 * 287.05 * T)
-        Cf_i= 0.455/ ((np.log10(Re))**2.58 * (1 + 0.144 * (V/a)**2)**0.65)
-    else:
-        Cf_i = 0.0
+        Cf_i= 0.455/ ((np.log10(Re))**2.58 * (1 + 0.144 * (V/a)**2)**0.65) * L*w/S_wing
     return Cf_i
 
-def cube_drag_coefficient(V, rho, h, S_wing):
-    if V>0:
+def cube_drag_coefficient(V, rho, h, S_wing, L):
+    if V <= 0:
+        CD_cube = 0
+        return CD_cube
+    else:
         T0 = 288.15
         T = T0 + -0.0065 * h
-        L_cube = 0.12
-        Re= rho * V * L_cube / 1.81e-5  # Reynolds number, assuming a kinematic viscosity of air at sea level
+        Re= rho * V * L / 1.81e-5  # Reynolds number, assuming a kinematic viscosity of air at sea level
         a= np.sqrt(1.4 * 287.05 * T)
-        CD_cube= (1.1 + 20/np.sqrt(Re)) * (1 + 0.15 * (V/a)**2) * L_cube**2/S_wing
-    else:
-        CD_cube = 0.0
+        CD_cube= (1.1 + 20/np.sqrt(Re)) * (1 + 0.15 * (V/a)**2) * L**2/S_wing
     return CD_cube
 
 def fuselage_drag_coefficient(L_n, L_c, Cf_fus, d, S_wing):
@@ -41,12 +41,12 @@ def fuselage_drag_coefficient(L_n, L_c, Cf_fus, d, S_wing):
     return CD_fus
 
 
-def calculate_thrust_UFC_FC(incline,V,rho, a, gamma_dot, W, V_vert_prop, CLmax, S_wing,aero_df, numberengines_vertical,numberengines_horizontal, propeller_wake_efficiency, CD_fus, CD_cube, Cf_blade, Cf_stab):
+def calculate_thrust_UFC_FC(incline,V,rho, a, gamma_dot, W, V_vert_prop, CLmax, S_wing,aero_df, numberengines_vertical,numberengines_horizontal, propeller_wake_efficiency, CD_fus, CD_gimbal, CD_speaker, CD_motor, Cf_blade, Cf_stab, Cf_poles):
     L_req = np.cos(incline)*W + W/g * V * gamma_dot #vertical force required for flight (stationary or not)
     if V > V_vert_prop:
         CL = 2*L_req/(rho*S_wing*V**2)
         CD_wing = np.interp(CL, aero_df["CL_corrected"], aero_df["CD_vlm"])
-        CD= CD_fus + CD_cube + CD_wing + 4 * Cf_blade + 3 * Cf_stab #Total drag coefficient
+        CD= CD_fus + CD_gimbal + CD_speaker + CD_wing + 4 * CD_motor + 2 * Cf_poles+ 4 * Cf_blade + 3 * Cf_stab #Total drag coefficient
         D_wing = 0.5*rho*CD*S_wing*V**2
         T_horizontal = ((D_wing + np.sin(incline)*W) + W/g * a)/ numberengines_horizontal #Thrust per horizontal propeller
         T_vertical = 0
@@ -54,23 +54,29 @@ def calculate_thrust_UFC_FC(incline,V,rho, a, gamma_dot, W, V_vert_prop, CLmax, 
     else:
         CL = CLmax
         CD_wing = np.interp(CL, aero_df["CL_corrected"], aero_df["CD_vlm"])
-        CD= CD_fus + CD_cube + CD_wing + 4 * Cf_blade + 3 * Cf_stab #Total drag coefficient
+        CD= CD_fus + CD_gimbal + CD_speaker + CD_wing + 4 * Cf_blade + 3 * Cf_stab #Total drag coefficient
         D_wing = 0.5*rho*CD*S_wing*V**2
         T_horizontal = ((D_wing + np.sin(incline)*W) + W/g * a)/ numberengines_horizontal #Thrust per horizontal propeller
         L_wing = 0.5*rho*CL*S_wing*V**2 * propeller_wake_efficiency  #Lifting force of the wing, parameter for wake of propellers
         L_prop = L_req - L_wing
         T_vertical = L_prop/numberengines_vertical #Thrust per vertical propeller
-           
-    return T_vertical,T_horizontal
+
+    if T_horizontal < 0:
+        T_horizontal = 0       
+    return T_vertical,T_horizontal, CD
 
 def calculate_power_FC(df_vertical,df_horizontal,incline,V,rho, a, gamma_dot, W, V_vert_prop, CLmax, S_wing, aero_df, numberengines_vertical,numberengines_horizontal, propeller_wake_efficiency,L_fus,L_n,L_c,d,L_blade,L_stab):
-    Cf_fus = flat_plate_drag_coefficient(V, rho, sva.altitude_from_density(rho), L_fus)
+    altitude = sva.altitude_from_density(rho)
+    Cf_blade= flat_plate_drag_coefficient(V, rho, altitude, S_wing, L_blade, w_blade)
+    Cf_stab = flat_plate_drag_coefficient(V, rho, altitude,S_wing,L_stab, w_stab)
+    Cf_poles = flat_plate_drag_coefficient(V, rho, altitude, S_wing, L_poles, w_poles)
+    Cf_fus = flat_plate_drag_coefficient(V, rho, altitude, S_wing, L_fus, w_fus)
+    CD_speaker = cube_drag_coefficient(V, rho, altitude, S_wing, L_speaker)
+    CD_gimbal = cube_drag_coefficient(V, rho, altitude, S_wing, L_gimbal)
+    CD_motor = cube_drag_coefficient(V, rho, altitude, S_wing, L_motor)
     CD_fus = fuselage_drag_coefficient(L_n, L_c, Cf_fus, d, S_wing)
-    CD_cube = cube_drag_coefficient(V, rho, sva.altitude_from_density(rho), S_wing)
-    Cf_blade = flat_plate_drag_coefficient(V, rho, sva.altitude_from_density(rho), L_blade)
-    Cf_stab = flat_plate_drag_coefficient(V, rho, sva.altitude_from_density(rho), L_stab)
 
-    T_vertical, T_horizontal = calculate_thrust_UFC_FC(incline,V,rho, a, gamma_dot, W, V_vert_prop, CLmax, S_wing,aero_df, numberengines_vertical,numberengines_horizontal, propeller_wake_efficiency, CD_fus, CD_cube, Cf_blade, Cf_stab)
+    T_vertical, T_horizontal, CD = calculate_thrust_UFC_FC(incline,V,rho, a, gamma_dot, W, V_vert_prop, CLmax, S_wing,aero_df, numberengines_vertical,numberengines_horizontal, propeller_wake_efficiency, CD_fus, CD_gimbal, CD_speaker, CD_motor, Cf_blade, Cf_stab, Cf_poles)
     
     #Vertical power
     max_thrust = df_vertical['Thrust_N'].max()
@@ -78,8 +84,7 @@ def calculate_power_FC(df_vertical,df_horizontal,incline,V,rho, a, gamma_dot, W,
         print(f"{T_vertical-max_thrust} Vertical thrust exceeded")
         T_vertical = max_thrust
         #raise ValueError(f"T_vertical ({T_vertical:.2f} N) exceeds the maximum thrust in the CSV ({max_thrust:.2f} N).")
-    if T_vertical<0:
-        P_vertical = 0
+    
     else: P_vertical = np.interp(T_vertical, df_vertical['Thrust_N'], df_vertical[' Power (W) '])*numberengines_vertical
     
     # Horizontal power
@@ -88,8 +93,7 @@ def calculate_power_FC(df_vertical,df_horizontal,incline,V,rho, a, gamma_dot, W,
         #print(f"{T_horizontal-max_thrust} Horizontal thrust exceeded")
         T_horizontal = max_thrust
         #raise ValueError(f"T_horizontal ({T_horizontal:.2f} N) exceeds the maximum thrust in the CSV ({max_thrust:.2f} N).")
-    if T_horizontal < 0:
-        P_horizontal = 0
+    
     else:
         P_horizontal = np.interp(T_horizontal, df_horizontal['Thrust_N'], df_horizontal[' Power (W) ']) * numberengines_horizontal
     P = P_vertical + P_horizontal
